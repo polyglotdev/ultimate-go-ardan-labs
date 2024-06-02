5f1ceddf83eca6dc8a76a3e60af37137f4a8246a
Dom Hallan <dom@domhallan.com>
2024-06-02 13:29:27 -0400

    Added for range example in arrays
    This commit introduces a new file that demonstrates the use of 'for range' in Go, specifically with arrays. The code creates an array of friends and then iterates over it using 'for range', altering some values during the iteration. It also includes print statements to show the original and altered values.

---
+++ lesson_3_arrays_range_mechanics/arrays/arrays.go
@@ -0,0 +1,29 @@
+// All material is licensed under the Apache License Version 2.0, January 2004
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Sample program to show how the for range has both value and pointer semantics.
+package main
+
+import "fmt"
+
+func main() {
+
+	// Using the pointer semantic form of the for range.
+	friends := [5]string{"Annie", "Betty", "Charley", "Doug", "Edward"}
+	fmt.Printf("Original value of friends[1]: %s\n", friends[1])
+
+	for i := range friends {
+		friends[1] = "Elijah"
+		friends[2] = "Ezra"
+
+		if i == 1 {
+			fmt.Printf("Altered value of friends[1]: %s\n", friends[1])
+		}
+		if i == 2 {
+			fmt.Printf("Altered value of friends[2]: %s\n", friends[2])
+		}
+	}
+
+	fmt.Println("friends:", friends)
+	fmt.Println("------------------------------")
+}
